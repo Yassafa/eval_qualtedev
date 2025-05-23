08e915f9d484ed51c1411190f1fbf1aa31a1f5e6
ESSAFA Yassine <12302332@q10409.iutv.univ-paris13.fr>
2025-05-23 11:26:21 +0200

    ajout question 3 et suppression de Mockito

--- TestStatistique.java
+++ TestStatistique.java
@@ -1,13 +1,9 @@
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 
 public class TestStatistique {
 
     @Test
     public void init() {
-        Calculatrice calc = Mockito.mock(Calculatrice.class);
-        when(calc.add(30, 50)).thenReturn(80);
-        when(calc.div(80, 2)).thenReturn(40);
+        Calculatrice calc = new Calculatrice();
     }
 }
