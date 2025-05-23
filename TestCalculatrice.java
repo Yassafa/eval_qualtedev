31f6b744c27ec86f34705b7f1b0e0efa71cea837
ESSAFA Yassine <12302332@q10409.iutv.univ-paris13.fr>
2025-05-23 10:31:45 +0200

    ajout gitignore et test addition

--- TestCalculatrice.java
+++ TestCalculatrice.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestCalculatrice {
+    private Calculatrice calc;
+
+    @BeforeEach
+    public void init() {
+        this.calc = new Calculatrice();
+    }
+
+    @Test
+    public void whenAddingTwoAndFive_ShouldReturnSeven() {
+        assertEquals(7, calc.add(2, 5));
+    }
 }
